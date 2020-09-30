048b87756451b23b2d83e90d0668de5ef4be7548
Ilya Venner <ilyavenner@gmail.com>
2020-09-30 22:34:42 +0300

    `structopt` + `log`

---
+++ src/init.rs
@@ -0,0 +1,50 @@
+use std::path::PathBuf;
+
+use structopt::StructOpt;
+
+/// An util which provides managing of the Minecraft Bedrock Server.
+#[derive(Debug, StructOpt)]
+pub struct Opt {
+    /// A telegram bot token.
+    pub token: String,
+
+    /// A path to the server binary.
+    pub path_to_binary: PathBuf,
+}
+
+/// Setups application logger.
+pub fn setup_logger() {
+    use fern::{
+        colors::{Color, ColoredLevelConfig},
+        Dispatch,
+    };
+
+    let colors_line = ColoredLevelConfig::new()
+        .error(Color::Red)
+        .warn(Color::Yellow)
+        .info(Color::White)
+        .debug(Color::White)
+        .trace(Color::BrightBlack);
+
+    let colors_level = colors_line.clone().info(Color::Green);
+
+    Dispatch::new()
+        .format(move |out, message, record| {
+            out.finish(format_args!(
+                "{color_line}[{date}] [{target}] [{level}{color_line}] {message}\x1B[0m",
+                color_line = format_args!(
+                    "\x1B[{}m",
+                    colors_line.get_color(&record.level()).to_fg_str()
+                ),
+                date = chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
+                target = record.target(),
+                level = colors_level.color(record.level()),
+                message = message,
+            ));
+        })
+        .level(log::LevelFilter::Warn)
+        .level_for("pluscrm_bot", log::LevelFilter::Trace)
+        .chain(std::io::stdout())
+        .apply()
+        .expect("cannot setup logger");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use init::{setup_logger, Opt};
+use structopt::StructOpt;
+
+mod init;
+
 fn main() {
-    println!("Hello, world!");
+    setup_logger();
+
+    let Opt {
+        token,
+        path_to_binary,
+    } = StructOpt::from_args();
+
+    println!("{}, {:?}", token, path_to_binary);
 }
